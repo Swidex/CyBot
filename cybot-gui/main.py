6dcb4e2a12d8c5fb7561c6fcd42153378a4c9e7e
Ben Hall <swidexx@gmail.com>
2021-10-19 15:45:28 -0500

    changed to header system

--- cybot-gui/main.py
+++ cybot-gui/main.py
@@ -13,6 +13,12 @@ CM_TO_PX = 1.724
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+HEADER_CODES = ["BRK\n","CMP\n","SCN\n","MOV\n","TRN\n","CAL\n","SRV\n"]
+
+# ERROR MSGS
+NO_HEADER_RECEIVED = "ERR: Did not receive header response."
+PREMATURE_FUNC_END = "ERR: Ended prematurely due to interruption to command."
+
 def polar_to_cart(deg, amt):
     """convert polar coordinates to cartesian"""
     x = float(amt) * math.sin(math.radians(int(deg)))
@@ -30,32 +36,23 @@ class Player():
         self.x = SCREEN_WIDTH / 2
         self.y = SCREEN_HEIGHT / 2
         self.servo_pos = 0
-        self.rot = 90
+        self.rot = 90.0
         self.size = 34.8 * CM_TO_PX
         self.rect = pygame.Rect(self.x-30,self.y-30,60,60)
-        self.progress_bar = pygame.Rect(0,0,0,0)
+        self.manual = True
+        self.bumper = ""
 
-    def move(self, reverse=False):
+    def move(self, amt):
         """move forward/backwards"""
-        if reverse:
-            cybot_uart.send_byte('s') # drive backwards
-        else:
-            cybot_uart.send_byte('w') # drive forward
-
-        amt = float(cybot_uart.read_line()) # wait for sensor distance for accuracy
+        
         x, y = polar_to_cart(self.rot, amt * CM_TO_PX)
         self.x += x
         self.y += y
         self.rect = pygame.Rect(self.x-(self.size/2),self.y-(self.size/2),self.size,self.size)
 
-    def turn(self, clockwise=False):
-        """turn"""
-        if clockwise:
-            cybot_uart.send_byte('d') # driveLeft
-        else:
-            cybot_uart.send_byte('a') # turnRight
-        angle = cybot_uart.read_line() # wait for sensor distance for accuracy
-        self.rot += int(angle)
+    def turn(self, angle):
+        """turn right/left"""
+        self.rot += angle
 
     def clear(self):
         self.x = SCREEN_WIDTH / 2
@@ -64,38 +61,26 @@ class Player():
         self.rect = pygame.Rect(self.x-30,self.y-30,60,60)
         objects.clear()
 
-    def scan(self):
+    def scan(self,theta,ir,ping):
         """scan 180 degrees infront"""
-        cybot_uart.send_byte('m')
-        while True:
-            # read from uart
-            data = cybot_uart.read_line()
 
-            # break if done scanning
-            if data == "Complete\n":
-                self.progress_bar = pygame.Rect(0,0,0,0)
-                break
-            
-            self.progress_bar = pygame.Rect(SCREEN_WIDTH/4, SCREEN_HEIGHT - 100, float(int(self.servo_pos) / 180) * (SCREEN_WIDTH / 2), 50)
-
-            # split data into readings
-            theta, ir, ping = data.rstrip().split(',')
-            self.servo_pos = int(theta)
-
-            distAvg = (float(ir) + float(ping)) / 2
-
-            # create new objects at coordinates
-            if float(ir) < 100: 
-                irx, iry = polar_to_cart(int(self.servo_pos) - 90 + self.rot, distAvg * CM_TO_PX)
-                offsetx, offsety = polar_to_cart(int(self.rot), float(34.8 / 2) * CM_TO_PX)
-                objects.append(Object(self.x+irx+offsetx, self.y+iry+offsety))    
+        self.servo_pos = theta
+        # create new objects at coordinates
+        if ir < 120:
+            irx, iry = polar_to_cart(int(self.servo_pos) - 90 + self.rot, ir * CM_TO_PX)
+            pgx, pgy = polar_to_cart(int(self.servo_pos) - 90 + self.rot, ping * CM_TO_PX)
+            offsetx, offsety = polar_to_cart(int(self.rot), float(34.8 / 2) * CM_TO_PX)
+            objects.append(Object(self.x+irx+offsetx, self.y+iry+offsety, self.x+pgx+offsetx, self.y+pgy+offsety,theta))    
 
 class Object():
     """class to hold scan data"""
 
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    def __init__(self,irx,iry,pgx,pgy,theta):
+        self.irx = irx
+        self.iry = iry
+        self.pgx = pgx
+        self.pgy = pgy
+        self.theta = theta
 
     def compare_to(self,object):
         dist = math.sqrt(pow((self.x - object.x),2) + pow(self.y - object.y,2))
@@ -106,15 +91,21 @@ class Object():
 # initalize serial connection
 try:
     cybot_uart = uart.UartConnection()
+    player = Player()
+    stream = threading.Thread(target=cybot_uart.data_stream, args=[player])
+    stream.daemon = True
+    stream.start()
 except serial.serialutil.SerialException:
     print("ERR: Failed to connect to CyBot.")
     sys.exit()
 
 # initalize pygame
 pygame.init()
+font = pygame.font.SysFont('Segoe UI', 30)
+
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 objects = []
-player = Player()
+rendered_objects = []
 
 running = True
 while running:
@@ -134,17 +125,17 @@ while running:
                 finally:
                     main = False
             if event.key == ord('a'): # turn left
-                player.turn()
+                cybot_uart.send_byte('a')
             if event.key == ord('d'): # turn right
-                player.turn(True)
+                cybot_uart.send_byte('d')
             if event.key == ord('w'): # move forward
-                player.move()
+               cybot_uart.send_byte('w')
             if event.key == ord('s'): # move backwards
-                player.move(True)
+                cybot_uart.send_byte('s')
             if event.key == ord('m'): # scan
-                scanThread = threading.Thread(target=player.scan)
-                scanThread.daemon = True
-                scanThread.start()
+                cybot_uart.send_byte('m')
+            if event.key == ord('t'): # toggle autonomous
+                cybot_uart.send_byte('t')
             if event.key == ord('k'): # clear
                 objects.clear()
 
@@ -156,12 +147,30 @@ while running:
     pygame.draw.circle(screen, WHITE, (player.x, player.y), player.size / 2)
     pygame.draw.arc(screen, GREEN, player.rect, math.radians(player.rot-180), math.radians(player.rot), 10)
     pygame.draw.line(screen, BLUE, (player.x, player.y), (player.x + xe, player.y + ye), 5)
-    pygame.draw.rect(screen, WHITE, player.progress_bar)
+    bump_text = font.render("bumper: " + player.bumper, False, WHITE)
+    screen.blit(bump_text,(0,40))
+    pos_text = font.render(str(round(player.x - (SCREEN_WIDTH / 2), 2)) + ", " + str(round(player.y - (SCREEN_HEIGHT / 2),2)) + ", " + str(round(player.rot,2)), False, WHITE)
+    screen.blit(pos_text,(0,80))
 
-    # draw sensor data
-    for object in objects:
-        pygame.draw.circle(screen, RED, (object.x, object.y), 2)
+    if player.manual:
+        mode_text = font.render("mode: manual", False, WHITE)
+    else:
+        mode_text = font.render("mode: auto", False, WHITE)
+    screen.blit(mode_text,(0,0))
 
-    
+    for x in objects:
+        pygame.draw.circle(screen, RED, (x.irx, x.iry), 2)
+
+    rendered_objects = []
+    start = 0 
+    for x in range(len(objects) - 1):
+        if abs(objects[x].theta - objects[x+1].theta) > 2 or (x + 1) == (len(objects) - 1):
+            rendered_objects.append(int((start + x) / 2))
+            start = x + 1
+
+    for i in rendered_objects:
+        pygame.draw.circle(screen, WHITE, [objects[i].pgx, objects[i].pgy], 4)
+
+
 
     pygame.display.flip()
--- cybot-gui/uart.py
+++ cybot-gui/uart.py
@@ -3,18 +3,32 @@ import serial
 class UartConnection:
     def __init__(self):
         """establish UART connection to cyBot and read output"""
-        self.ser = serial.serial_for_url("socket://192.168.1.1:288") 
+        self.ser = serial.serial_for_url("socket://192.168.1.1:288", baudrate=115200) 
 
-    def read_line(self):
-        """read until new line is detected"""
-        data = ""
-        last_byte = ''
+    def data_stream(self, player):
+        """interpret information from uart"""
         while True:
-            if last_byte == "\n":
-                return data
-            last_byte = self.ser.read(1).decode("utf-8")
-            data += last_byte
-            self.ser.flushOutput()
+            data = self.ser.read_until().decode("utf-8")
+            print(data.rstrip())
+            data = data.split(",")
+            if "MOV" == data[0]:
+                player.move(float(data[1]))
+            elif "TRN" == data[0]:
+                player.turn(float(data[1]))
+            elif "SCN" == data[0]:
+                player.scan(int(data[1]),float(data[2]),float(data[3]))
+            elif "BMP" == data[0]:
+                data[1] = int(data[1])
+                if data[1] > 0:
+                    player.bumper = "right"
+                elif data[1] < 0:
+                    player.bumper = "left"
+                elif data[1] == 0:
+                    player.bumper = ""
+            elif "MAN" == data[0]:
+                player.manual = True
+            elif "AUT" == data[0]:
+                player.manual = False
 
     def send_byte(self,data):
         data = bytes(data, 'utf-8')
