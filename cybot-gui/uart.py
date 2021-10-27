c8a9dfb44311b74a65846d8afe3414f36b13defd
Ben Hall <swidexx@gmail.com>
2021-10-27 17:02:19 -0500

    updated uart protocol

--- cybot-gui/uart.py
+++ cybot-gui/uart.py
@@ -4,6 +4,7 @@ class UartConnection:
     def __init__(self):
         """establish UART connection to cyBot and read output"""
         self.ser = serial.serial_for_url("socket://192.168.1.1:288", baudrate=115200)
+        self.waiting = False
 
     def data_stream(self, player):
         """interpret information from uart"""
@@ -11,14 +12,13 @@ class UartConnection:
             data = self.ser.read_until().decode("utf-8").split(",")
             print(data)
             if "MOV" == data[0]:
-                player.move(float(data[1]))
+                player.update(0, float(data[1]))
                 self.waiting = False
             elif "TRN" == data[0]:
-                player.turn(float(data[1]))
+                player.update(float(data[1]), 0)
                 self.waiting = False
             elif "SCN" == data[0]:
-                player.scan(int(data[1]),float(data[2]),float(data[3]))
-                self.waiting = False
+                player.scan(float(data[1]),float(data[2]))
             elif "BMP" == data[0]:
                 data[1] = int(data[1])
                 if data[1] > 0:
@@ -27,10 +27,6 @@ class UartConnection:
                     player.bumper = "left"
                 elif data[1] == 0:
                     player.bumper = ""
-            elif "MAN" == data[0]:
-                player.manual = True
-            elif "AUT" == data[0]:
-                player.manual = False
 
     def send_data(self,data):
         for x in range(len(data)):
