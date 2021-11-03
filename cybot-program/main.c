1fccdb1251d7bdb0eb3bfed05c58f88221e33770
Ben Hall <swidexx@gmail.com>
2021-11-03 12:43:20 -0500

    new servo library

--- cybot-program/main.c
+++ cybot-program/main.c
@@ -6,43 +6,35 @@
 #include "timer.h"
 #include "lcd.h"
 #include "open_interface.h"
-#include "movement.h"
 #include "uart.h"
 #include "adc.h"
 #include "ping.h"
+#include "servo.h"
 
-#define MAX_OBJECTS 10
-#define MAX_DIST 800
-#define SCAN_INTERVAL 2
 #define PING_MOD 969.33
 
-int uartTX_bump(int status, oi_t *sensor_data , char uartTX[20]) {
-    /*if (status != 1 && sensor_data->bumpLeft) {
-        sendUartString("BMP,-1\n");
-        status = 1;
-    } else if (status != 2 && sensor_data->bumpRight) {
-        sendUartString("BMP,1\n");
-        status = 2;
-    } else if (status > 0 && !(sensor_data->bumpRight || sensor_data->bumpLeft)){
-        sendUartString("BMP,0\n");
-        status = 0;
-    }*/
-    return status;
+void scanInfront(char* uartTX)
+{
+    lcd_clear();
+    sprintf(uartTX, "SCN,%0.0f,%d,%0.2f\n",servo_pos,adc_read(),ping_read() / PING_MOD);
+    sendUartString(uartTX);
+    lcd_puts(uartTX);
 }
 
 void main() {
     lcd_init();
-
     lcd_clear();
     lcd_puts("Initializing...");
 
     uart_init(115200);
     adc_init();
     ping_init();
+    servo_init();
+    servo_move(90.0);
+
     oi_t *sensor_data = oi_alloc();
     oi_init(sensor_data);
 
-    int status = 0;             // var for bumper status
     char uartRX;                // var to hold uart RX data
     char uartTX[20] = "";       // string to hold uart TX data
 
@@ -54,7 +46,6 @@ void main() {
         oi_update(sensor_data);
         oi_setWheels(0, 0);
 
-        status = uartTX_bump(status, sensor_data, uartTX); // bumper data
         uartRX = receive_data; // set local variable
         receive_data = '\0'; // clear RX for interrupts
 
@@ -126,10 +117,17 @@ void main() {
             }
             case 'm': // scan
             {
-                lcd_clear();
-                sprintf(uartTX, "SCN,%d,%0.2f\n",adc_read(),ping_read() / PING_MOD);
-                sendUartString(uartTX);
-                lcd_puts(uartTX);
+                servo_move(90.0);
+                scanInfront(uartTX);
+                break;
+            }
+            case 'n': // scan
+            {
+                float i;
+                for ( i = 0.0; i < 180; i++) {
+                    scanInfront(uartTX);
+                    servo_move(i);
+                }
                 break;
             }
         }
